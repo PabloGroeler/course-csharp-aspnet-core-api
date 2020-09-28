a218b68e52c330d877a5779ad12789836f377e26
Pablo Groeler <pablo_groeler@hotmail.com>
2020-09-28 07:51:21 -0400

    swagger

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace application
 {
@@ -30,6 +31,23 @@ namespace application
       ConfigureService.ConfigureDependencysService(services);
       ConfigureRepository.ConfigureDependencyRepository(services);
       services.AddControllers();
+      services.AddSwaggerGen(c => {
+        c.SwaggerDoc("v1", new OpenApiInfo{
+          Version = "v1",
+          Title = "Curso de API com AspNetCore 3.1 - Na Prática",
+          Description = "Arquitetura DDD",
+          TermsOfService = new Uri("http://www.google.com"),
+          Contact = new OpenApiContact {
+            Name = "Pablo Henrique Groeler",
+            Email = "pablo_groeler@hotmail.com",
+            Url = new Uri("Http://www.google.com")
+          },
+          License = new OpenApiLicense {
+            Name = "Termo de licença de Uso",
+            Url = new Uri("http://www.google.com.br")
+          }
+        }); 
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +59,12 @@ namespace application
       }
 
       app.UseHttpsRedirection();
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c => {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Curso de API com AspNetCore 3.1");
+        c.RoutePrefix = string.Empty;
+      });
 
       app.UseRouting();
 
