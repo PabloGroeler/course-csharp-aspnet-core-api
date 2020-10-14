02234b32b73bdba52f69baf90bbfa250e7cc2796
Pablo Groeler <pablo_groeler@hotmail.com>
2020-10-14 13:39:03 -0400

    - Integration Test

--- src/Api.Application/Startup.cs
+++ src/Api.Application/Startup.cs
@@ -66,12 +66,6 @@ namespace application
             var signingConfigurations = new SigningConfigurations();
             services.AddSingleton(signingConfigurations);
 
-            var tokenConfigurations = new TokenConfiguration();
-            new ConfigureFromConfigurationOptions<TokenConfiguration>(
-              Configuration.GetSection("TokenConfigurations"))
-                .Configure(tokenConfigurations);
-            services.AddSingleton(tokenConfigurations);
-
             services.AddAuthentication(authOptions =>
             {
                 authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -80,8 +74,8 @@ namespace application
             {
                 var paramsValidation = bearerOptions.TokenValidationParameters;
                 paramsValidation.IssuerSigningKey = signingConfigurations.Key;
-                paramsValidation.ValidAudience = tokenConfigurations.Audience;
-                paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+                paramsValidation.ValidAudience = Environment.GetEnvironmentVariable("Audience");
+                paramsValidation.ValidIssuer = Environment.GetEnvironmentVariable("Issuer");
                 paramsValidation.ValidateIssuerSigningKey = true;
                 paramsValidation.ValidateLifetime = true;
                 paramsValidation.ClockSkew = TimeSpan.Zero;
--- src/Api.Integration.Test/BaseIntegration.cs
+++ src/Api.Integration.Test/BaseIntegration.cs
@@ -1,16 +1,20 @@
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using Api.CrossCutting.Mappings;
 using Api.Data.Context;
+using Api.Domain.Dtos;
 using application;
 using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Api.Integration.Test
 {
-    public class BaseIntegration
+    public abstract class BaseIntegration: IDisposable
     {
         public MyContext myContext { get; private set; }
         public HttpClient client { get; private set; }
@@ -30,6 +34,33 @@ namespace Api.Integration.Test
             myContext.Database.Migrate();
 
             mapper = new AutoMapperFixture().GetMapper();
+
+            client = server.CreateClient();
+        }
+
+        public async Task AdicionarToken() {
+            var loginDto = new LoginDto()
+            {
+                Email = "Pablo@celtasistemas.com.br"
+            };
+
+            var resultLogin = await PostJsonAsync(loginDto, $"{hostApi}login", client);
+            var jsonLogin = await resultLogin.Content.ReadAsStringAsync();
+            var loginObject = JsonConvert.DeserializeObject<LoginResponseDto>(jsonLogin);
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", 
+                                                         loginObject.acessToken);
+        }
+
+        public static async Task<HttpResponseMessage> PostJsonAsync(object dataclass, string url, HttpClient client) {
+            return await client.PostAsync(url, 
+                new StringContent(JsonConvert.SerializeObject(dataclass), System.Text.Encoding.UTF8, "application/json"));
+        }
+
+        public void Dispose()
+        {
+            myContext.Dispose();
+            client.Dispose();
         }
     }
 
